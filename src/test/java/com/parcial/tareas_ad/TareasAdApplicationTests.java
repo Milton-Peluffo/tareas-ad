9815750b93b23e5ecceea08f562faf8819775487
Milton-Peluffo <milton.peluffoc@gmail.com>
2026-04-18 11:29:12 -0500

    initial commit, upload proyect base

---
+++ src/test/java/com/parcial/tareas_ad/TareasAdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcial.tareas_ad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareasAdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
